4830d1ac774dbf6b9f138b8d16c4299ca1c92195
Kristián Feldsam <feldsam@gmail.com>
2015-10-16 13:17:18 +0200

    Refactored task log and locking mechanismus
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- classes/refine.php
+++ classes/refine.php
@@ -23,8 +23,21 @@ namespace Oil;
  */
 class Refine
 {
+	protected static $_task_log_enabled;
+	protected static $_hostname;
+	protected static $_params;
+	protected static $_task_log_id;
+	
 	public static function run($task, $args = array())
 	{
+		// task log init
+		static::$_task_log_enabled = static::is_task_log_enabled();
+		if(static::$_task_log_enabled)
+		{
+			static::$_hostname = isset($_SERVER['FUEL_HOSTNAME']) ? $_SERVER['FUEL_HOSTNAME'] : '';
+			static::$_params   = json_encode(array_slice($_SERVER['argv'], 3));
+		}
+		
 		$task = strtolower($task);
 
 		// Make sure something is set
@@ -103,44 +116,11 @@ class Refine
 			is_callable($task.'::_init') and $task::_init();
 		}
 
-		//Check if logs exists
-		if (isset($_SERVER['FUEL_ENABLE_TASK_LOG']) and \DBUtil::table_exists('task_log'))
+		// Lock task
+		if(static::$_task_log_enabled and ! static::task_lock($task))
 		{
-			$hostname = isset($_SERVER['FUEL_HOSTNAME']) ? $_SERVER['FUEL_HOSTNAME'] : '';
-
-			$params   = json_encode(array_slice($_SERVER['argv'], 3));
-
-			//Check if task don't running
-			if ($prev = \DB::select()->from('task_log')->where('task', $task)->where('params', $params)->where('hostname', $hostname)->order_by('id', 'desc')->limit(1)->execute()->as_array())
-			{
-				$prev = $prev[0];
-				//If task is longer than 15 minutes
-				if ($prev['status'] == 'running' and $prev['created_at'] < (time() - 60 * 15))
-				{
-					$prev['status']    = 'error';
-					$prev['error_message']     = 'Timeout: Task was running over 15 minutes';
-					$prev['finish_at'] = time();
-					\DB::update('task_log')->set($prev)->where('id', $prev['id'])->execute();
-				}
-
-				// If task is active, stop this cron
-				if ($prev['status'] == 'running')
-				{
-					\Cli::write('The same task still running.');
-					return;
-				}
-			}	
-
-			//Create new Cron log
-			$log             = array();
-			$log['status']   = 'running';
-			$log['created_at']   = time();
-			$log['hostname'] = $hostname;
-			$log['task']     = $task;
-			$log['params']   = $params;
-
-			// get inserted log ID
-			$log_id = \DB::insert('task_log')->set($log)->execute()[0];
+			\Cli::write('Task_log: Task is already running...');
+			return;
 		}
 
 		if (is_callable(array($new_task, $method)))
@@ -151,37 +131,122 @@ class Refine
 				{
 					\Cli::write($return);
 				}
-				isset($log) and $log['status'] = 'ok';
+				
+				// unlock task
+				static::$_task_log_enabled and static::task_unlock();
 			}
-			catch (Exception $e)
+			catch (\Exception $e)
 			{
-				isset($log) and $log['status'] = 'error';
-				isset($log) and $log['error_message'] = $e->getMessage();
+				// unlock task with error
+				static::$_task_log_enabled and static::task_unlock($e->getMessage());
 			}
-
-			isset($log) and $log['finish_at'] = time();
-			isset($log) and \DB::update('task_log')->set($log)->where('id', $log_id)->execute();
+			
+			return;
 		}
-		else
+		
+		\Cli::write(sprintf('Task "%s" does not have a command called "%s".', $task, $method));
+
+		\Cli::write("\nDid you mean:\n");
+		$reflect = new \ReflectionClass($new_task);
+
+		// Ensure we only pull out the public methods
+		$methods = $reflect->getMethods(\ReflectionMethod::IS_PUBLIC);
+		if (count($methods) > 0)
 		{
-			\Cli::write(sprintf('Task "%s" does not have a command called "%s".', $task, $method));
-
-			\Cli::write("\nDid you mean:\n");
-			$reflect = new \ReflectionClass($new_task);
-
-			// Ensure we only pull out the public methods
-			$methods = $reflect->getMethods(\ReflectionMethod::IS_PUBLIC);
-			if (count($methods) > 0)
+			foreach ($methods as $method)
 			{
-				foreach ($methods as $method)
+				if (strpos($method->name, '_') !== 0)
 				{
-					if (strpos($method->name, '_') !== 0)
-					{
-						\Cli::write(sprintf("php oil [r|refine] %s:%s", $reflect->getShortName(), $method->name));
-					}
+					\Cli::write(sprintf("php oil [r|refine] %s:%s", $reflect->getShortName(), $method->name));
 				}
 			}
 		}
+	}
+
+	protected static function is_task_log_enabled()
+	{
+		return (isset($_SERVER['FUEL_ENABLE_TASK_LOG']) and $_SERVER['FUEL_ENABLE_TASK_LOG'] == 1 and \DBUtil::table_exists('task_log'));
+	}
+	
+	protected static function is_task_locked($task)
+	{
+		// select task from db
+		$task_log = \DB::select()->from('task_log')->where('task', $task)->where('params', static::$_params)->where('hostname', static::$_hostname)->order_by('id', 'desc')->limit(1)->execute()->as_array();
+		
+		// task doesn't exists, so lock is inactive
+		if( ! $task_log) return false;
+
+		// task exists
+		$task_log = $task_log[0];
+		
+		// handle locked task for more that 15min, so unlock
+		if ($task_log['status'] == 'running' and $task_log['created_at'] < (time() - 60 * 15))
+		{
+			$task_log['status']        = 'error';
+			$task_log['error_message'] = 'Timeout: Task was running over 15 minutes';
+			$task_log['finish_at']     = time();
+			
+			\DB::update('task_log')->set($task_log)->where('id', $task_log['id'])->execute();
+			
+			return false;
+		}
+
+		// Task is not running so lock is inactive
+		if ($task_log['status'] != 'running') return false;
+		
+		return true;
+	}
+	
+	protected static function task_lock($task)
+	{
+		// task is already locked
+		if(static::is_task_locked($task))
+		{
+			return false;
+		}
+		
+		// task is unlocked, so create lock record
+		$log = array(
+			'status'     => 'running',
+			'created_at' => time(),
+			'hostname'   => static::$_hostname,
+			'task'       => $task,
+			'params'     => static::$_params,
+		);
+
+		$result = \DB::insert('task_log')->set($log)->execute();
+		
+		if($result)
+		{
+			static::$_task_log_id = $result[0];
+			return true;
+		}
+		
+		return false;
+	}
+	
+	protected static function task_unlock($error = null)
+	{
+		// on error
+		if($error)
+		{
+			$log = array(
+				'status'        => 'error',
+				'error_message' => $error,
+				'finish_at'     => time(),
+			);
+			
+			\DB::update('task_log')->set($log)->where('id', static::$_task_log_id)->execute();
+			
+			return;
+		}
+		
+		$log = array(
+			'status'        => 'ok',
+			'finish_at'     => time(),
+		);
+		
+		\DB::update('task_log')->set($log)->where('id', static::$_task_log_id)->execute();
 	}
 
 	public static function help()
